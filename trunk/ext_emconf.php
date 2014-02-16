97c568cab3ab95d7e4840cb0b1178145d3a2513e
renestaeker <renestaeker@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-02-16 17:14:21 +0000

    release 0.2.4
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rs_securedownload@83797 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_emconf.php
+++ trunk/ext_emconf.php
@@ -1,8 +1,9 @@
 <?php
+
 /***************************************************************
  * Extension Manager/Repository config file for ext "rs_securedownload".
  *
- * Auto generated 18-05-2013 14:06
+ * Auto generated 16-02-2014 18:07
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -14,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Display Downloads only with correct code',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.2.3',
+	'version' => '0.2.4',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -41,6 +42,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:29:{s:21:"ext_conf_template.txt";s:4:"1a42";s:12:"ext_icon.gif";s:4:"88fe";s:17:"ext_localconf.php";s:4:"a838";s:14:"ext_tables.php";s:4:"b582";s:14:"ext_tables.sql";s:4:"7acb";s:19:"flexform_ds_pi1.xml";s:4:"21c7";s:34:"icon_tx_rssecuredownload_codes.gif";s:4:"88fe";s:13:"locallang.xml";s:4:"bb0e";s:16:"locallang_db.xml";s:4:"b8fb";s:20:"rssecuredownload.php";s:4:"8c9e";s:7:"tca.php";s:4:"3be6";s:14:"doc/manual.sxw";s:4:"4384";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"bd4e";s:14:"mod1/index.php";s:4:"2ad0";s:18:"mod1/locallang.xml";s:4:"b045";s:22:"mod1/locallang_mod.xml";s:4:"7541";s:19:"mod1/moduleicon.gif";s:4:"88fe";s:14:"pi1/ce_wiz.gif";s:4:"44d4";s:37:"pi1/class.tx_rssecuredownload_pi1.php";s:4:"ef3b";s:45:"pi1/class.tx_rssecuredownload_pi1_wizicon.php";s:4:"2eb4";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"9ccc";s:24:"pi1/static/editorcfg.txt";s:4:"cd12";s:20:"pi1/static/setup.txt";s:4:"6c69";s:14:"res/export.gif";s:4:"bd0e";s:15:"res/garbage.gif";s:4:"90c6";s:13:"res/minus.gif";s:4:"e4ff";s:12:"res/plus.gif";s:4:"cdba";}',
+	'_md5_values_when_last_written' => 'a:31:{s:9:"ChangeLog";s:4:"8aa7";s:21:"ext_conf_template.txt";s:4:"1a42";s:12:"ext_icon.gif";s:4:"88fe";s:17:"ext_localconf.php";s:4:"32e5";s:14:"ext_tables.php";s:4:"cdcb";s:14:"ext_tables.sql";s:4:"c855";s:19:"flexform_ds_pi1.xml";s:4:"21c7";s:34:"icon_tx_rssecuredownload_codes.gif";s:4:"88fe";s:13:"locallang.xml";s:4:"bb0e";s:16:"locallang_db.xml";s:4:"b8fb";s:20:"rssecuredownload.php";s:4:"8e43";s:7:"tca.php";s:4:"2a23";s:14:"doc/manual.sxw";s:4:"01ea";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"bd4e";s:14:"mod1/index.php";s:4:"6265";s:18:"mod1/locallang.xml";s:4:"b045";s:22:"mod1/locallang_mod.xml";s:4:"7541";s:19:"mod1/moduleicon.gif";s:4:"88fe";s:14:"pi1/ce_wiz.gif";s:4:"44d4";s:37:"pi1/class.tx_rssecuredownload_pi1.php";s:4:"872b";s:45:"pi1/class.tx_rssecuredownload_pi1_wizicon.php";s:4:"7bb9";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"4090";s:16:"pi1/template.htm";s:4:"ebee";s:24:"pi1/static/editorcfg.txt";s:4:"cd12";s:20:"pi1/static/setup.txt";s:4:"6c69";s:14:"res/export.gif";s:4:"bd0e";s:15:"res/garbage.gif";s:4:"90c6";s:13:"res/minus.gif";s:4:"e4ff";s:12:"res/plus.gif";s:4:"cdba";}',
+	'suggests' => array(
+	),
 );
+
 ?>
