d26fb9f5254cc43e1ac399bd81be7cbf6bde12b9
renestaeker <renestaeker@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-07-29 15:11:27 +0000

    tagged 0.3.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rs_securedownload/tags/0.3.0@86056 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ rs_securedownload/ext_emconf.php
@@ -0,0 +1,50 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "rs_securedownload".
+ *
+ * Auto generated 29-07-2014 17:02
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Secure Download',
+	'description' => 'Display Downloads only with correct code',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '0.3.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => 'mod1',
+	'state' => 'stable',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Rene',
+	'author_email' => 'typo3@rs-softweb.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5.0-6.2.99',
+			'php' => '5.2.0-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:31:{s:9:"ChangeLog";s:4:"d41d";s:21:"ext_conf_template.txt";s:4:"1a42";s:12:"ext_icon.gif";s:4:"88fe";s:17:"ext_localconf.php";s:4:"32e5";s:14:"ext_tables.php";s:4:"8aa8";s:14:"ext_tables.sql";s:4:"c855";s:19:"flexform_ds_pi1.xml";s:4:"21c7";s:34:"icon_tx_rssecuredownload_codes.gif";s:4:"88fe";s:13:"locallang.xml";s:4:"bb0e";s:16:"locallang_db.xml";s:4:"b8fb";s:20:"rssecuredownload.php";s:4:"8e43";s:7:"tca.php";s:4:"2a23";s:14:"doc/manual.sxw";s:4:"01ea";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"bd4e";s:14:"mod1/index.php";s:4:"a5a7";s:18:"mod1/locallang.xml";s:4:"4e35";s:22:"mod1/locallang_mod.xml";s:4:"7541";s:19:"mod1/moduleicon.gif";s:4:"88fe";s:14:"pi1/ce_wiz.gif";s:4:"44d4";s:37:"pi1/class.tx_rssecuredownload_pi1.php";s:4:"2365";s:45:"pi1/class.tx_rssecuredownload_pi1_wizicon.php";s:4:"7bb9";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"4090";s:16:"pi1/template.htm";s:4:"ebee";s:24:"pi1/static/editorcfg.txt";s:4:"cd12";s:20:"pi1/static/setup.txt";s:4:"6c69";s:14:"res/export.gif";s:4:"bd0e";s:15:"res/garbage.gif";s:4:"90c6";s:13:"res/minus.gif";s:4:"e4ff";s:12:"res/plus.gif";s:4:"cdba";}',
+	'suggests' => array(
+	),
+);
+
+?>
---
+++ rs_securedownload/ext_tables.php
@@ -0,0 +1,43 @@
+<?php
+if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+$TCA['tx_rssecuredownload_codes'] = array (
+	'ctrl' => array (
+		'title'     => 'LLL:EXT:rs_securedownload/locallang_db.xml:tx_rssecuredownload_codes',
+		'label'     => 'title',
+		'tstamp'    => 'tstamp',
+		'crdate'    => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'default_sortby' => 'ORDER BY crdate',
+		'delete' => 'deleted',
+		'enablecolumns' => array (
+			'disabled' => 'hidden',
+			'starttime' => 'starttime',
+			'endtime' => 'endtime',
+		),
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
+		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY).'icon_tx_rssecuredownload_codes.gif',
+	),
+	'feInterface' => array (
+		'fe_admin_fieldList' => 'hidden, starttime, endtime, title, description, codeprompt, code, file',
+	)
+);
+
+if (TYPO3_MODE == 'BE')	{
+	t3lib_extMgm::addModule('web', 'txrssecuredownloadM1', '', t3lib_extMgm::extPath($_EXTKEY).'mod1/');
+}
+
+if (version_compare(TYPO3_branch, '6.1', '<')) {
+	t3lib_div::loadTCA('tt_content');
+}
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key,pages,starttime,endtime';
+// you add pi_flexform to be renderd when your plugin is shown
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1']='pi_flexform';
+
+t3lib_extMgm::addPlugin(array('LLL:EXT:rs_securedownload/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'), 'list_type');
+
+t3lib_extMgm::addStaticFile($_EXTKEY, 'pi1/static/', 'Secure Download');
+
+t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:rs_securedownload/flexform_ds_pi1.xml');
+
+if (TYPO3_MODE=='BE')	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_rssecuredownload_pi1_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_rssecuredownload_pi1_wizicon.php';
+?>
